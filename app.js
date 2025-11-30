1389182319e4dd4c559dd54774057361a5ce80f9
Noam Slezack <145605933+nonolasaire@users.noreply.github.com>
2025-11-30 13:04:49 +0800

    refactor: Clean up duplicated routes, add reusable helpers and centralized CSS"

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
-var quizRouter = require('./routes/quiz');
 var quizzesRouter = require('./routes/quizzes');
 var apiRouter = require('./routes/api'); 
 var app = express();
@@ -26,12 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/', authRouter);
-app.use('/',quizzesRouter);
-app.use('/',quizRouter);
-app.use('/api', apiRouter);
+app.use('/', authRouter);          // Login, register, logout
+app.use('/', quizzesRouter);       // Toutes les routes quiz (instructor + student)
+app.use('/api', apiRouter);        // API JSON pour Vue.js
 
 
 // catch 404 and forward to error handler
